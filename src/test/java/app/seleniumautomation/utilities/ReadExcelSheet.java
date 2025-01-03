a7035699477b43828a942e7b7197dd8ba5d040d3
shivatechhub <shivatechhub@gmail.com>
2025-01-03 09:40:03 +0530

    commiting the latest file into the local branch

---
+++ src/test/java/app/seleniumautomation/utilities/ReadExcelSheet.java
@@ -0,0 +1,16 @@
+package app.seleniumautomation.utilities;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class ReadExcelSheet {
+	public static void getExcelWorkBook(String path) throws FileNotFoundException {
+		FileInputStream fs = new FileInputStream(new File(path));
+		
+		
+	}
+}
